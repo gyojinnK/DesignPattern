709e0c59a185e6f2a8aaa9c6e9bae2fd99088b31
gyojin <ksmug1998@gmail.com>
2025-10-16 14:38:17 +0900

    [DesignPattern] Singleton

--- dist/grimpan.js
+++ dist/grimpan.js
@@ -1,17 +1,7 @@
-/** 심볼을 사용하여 private 생성자를 구현할 수 있음
- * Symbol() === Symbol() => false (항상 새로운 심볼을 생성)
- * Symbol.for('key') === Symbol.for('key') => true (같은 키로 심볼을 공유)
- * 따라서, new Grimpan()을 방지하기 위해 심볼을 사용
- */
-const GRIMPAN_CONSTRUCTOR_SYMBOL = Symbol();
-
 // singleton 패턴 적용
 class Grimpan {
     static instance;
-    constructor(canvas, symbol) {
-        if(symbol !== GRIMPAN_CONSTRUCTOR_SYMBOL) {
-            throw new Error(`new를 통해서 호출할 수 없습니다.`);
-        }
+    constructor(canvas) {
         if (!canvas || !(canvas instanceof HTMLCanvasElement)) {
             throw new Error('Canvas 엘리먼트를 입력하세요.');
         }
@@ -20,7 +10,7 @@ class Grimpan {
     initializeMenu() { }
     static getInstance() {
         if (!this.instance) {
-            this.instance = new Grimpan(document.querySelector('#canvas'), GRIMPAN_CONSTRUCTOR_SYMBOL);
+            this.instance = new Grimpan(document.querySelector('#canvas'));
         }
         return this.instance;
     }
